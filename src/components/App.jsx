da20f9b692f1139f15eab85b0c40e1671cf86e42
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-12-25 02:42:24 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  addContact = contact => {
+    this.setState(prevState => prevState.contacts.push(contact));
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Phonebook">
+          <form>
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+            <button onClick={e => this.addContact('vv')}>Add contact</button>
+          </form>
+        </Section>
+      </div>
+    );
+  }
+}
